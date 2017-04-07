dbe28f2b237f29e4ad4886c00402a25c281d3f64
spenc <spenc@153.90.55.169>
2017-04-07 10:18:01 -0600

    Imported libs for class

--- TravellingSalesman/src/TSP.java
+++ TravellingSalesman/src/TSP.java
@@ -2,6 +2,7 @@
 public class TSP {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!"); 
+		int num = 10; // This is the number That we will use until I figure out commandline args again
+		
 	}
 }
